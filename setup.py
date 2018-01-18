3884c5d3645200fc4fa7176e18144ecc51558e5e
initbar <initbar@protonmail.ch>
2018-01-18 03:02:55 -0500

    add word

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     author='Herbert <initbar> Shin',
     author_email='noreply@init.bar',
-    description="Hierarchial Multiple Dictionary",
+    description="Hierarchial Multiple Dictionary engine",
     license='MIT',
     long_description='hmd is the next-generation Hierarchial Multiple Dictionary (HMD) compiler, pattern matching, and text traversal engine.',
     name='hmd',
